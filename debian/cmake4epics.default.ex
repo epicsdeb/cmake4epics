d9a3ac24ee3892734b74e13e674aa2a162736be7
Martin Konrad <konrad@frib.msu.edu>
2018-02-06 10:35:11 -0500

    Create debian directory
    dh_make --single -p cmake4epics_1.0 --copyright bsd

---
+++ debian/cmake4epics.default.ex
@@ -0,0 +1,10 @@
+# Defaults for cmake4epics initscript
+# sourced by /etc/init.d/cmake4epics
+# installed at /etc/default/cmake4epics by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(cmake4epics):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="cmake4epics" command="/usr/bin/cmake4epics"
